39a5b0336aee859c9e957a1bce8c9d3de54c7bde
Emilien <emilien.wauquier@gmail.com>
2017-05-03 00:50:41 +0200

    Ajouté la prise en charge des commandes via les messages

--- events/message.js
+++ events/message.js
@@ -1,10 +1,67 @@
+import fs from 'fs';
+import Log from '../libs/log';
+import config from '../config.json';
+
+const log = new Log(process.env.NODE_ENV);
+
 module.exports = function (bot) {
   bot.on('message', (message) => {
-    console.log(message.content);
+    console.log('[MESSAGE]', message.content); // eslint-disable-line no-console
 
-    if (message.content === 'ping') {
-      message.reply('pong');
-      message.channel.sendMessage('pong');
+    /**
+     *
+     * Gestion des commandes.
+     *
+     */
+
+    // Si le message est une commande qui n'est pas envoyée par le bot.
+    if (message.content.startsWith(config.command_prefix)
+        && !message.author.bot) {
+      // On met tout en minuscule pour éviter les problèmes de casse.
+      const input = message.content.toLowerCase();
+
+      // Récupération de la commande et de son argument.
+      const trigger = input.match(/\/([0-9a-z]+)/)[1];
+
+      log.verbose(`Commande <${trigger}> demandée...`);
+
+      try {
+        // Récupération de la liste des commandes.
+        let files = fs.readdirSync(`${__dirname}/../commands/`);
+
+        // Suppression du fichier index.js du tableau.
+        files = files.filter(item => item !== 'index.js');
+
+        let command = false;
+
+        files.some((file) => {
+          const properties = require(`${__dirname}/../commands/${file}/properties.json`);
+
+          if (properties.triggers.includes(trigger)) {
+            log.verbose(`Déclencheur trouvé pour la commande <${file}> [${properties.triggers}]`);
+
+            command = require(`${__dirname}/../commands/${file}/command.js`);
+            return true; // on sort
+          }
+
+          return false; // on continue
+        });
+
+        // Si la commande a été trouvée, on l'exécute.
+        if (command) {
+          command.execute(message)
+            .then((reply) => {
+              message.reply(reply);
+            })
+            .catch((error) => {
+              log.error(`L'execution de la commande a provoqué une erreur : ${error}`);
+            });
+        } else {
+          log.verbose(`Aucune commande trouvée pour ${trigger}.`);
+        }
+      } catch (error) {
+        log.error(`La lecture des propriétés de la commande a provoqué une erreur : ${error}`);
+      }
     }
   });
 };
