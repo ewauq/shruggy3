4fb45ec18e1ef48e6cbf4e1bc73561a1d611b7ae
Emilien <emilien.wauquier@gmail.com>
2017-05-03 23:18:32 +0200

    Refactorisé le code

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import Discord from 'discord.js';
-import colors from 'colors/safe';
 import Events from './events/';
 import Log from './libs/log';
 import config from './config.json';
-
 
 const bot = new Discord.Client();
 const events = new Events(bot);
@@ -11,13 +9,16 @@ const log = new Log(process.env.NODE_ENV);
 
 log.info('Lancement du bot en cours...');
 
-Promise.all([events.load(), bot.login(config.token)])
-  .then((debug) => {
-    log.info('Le bot est maintenant connecté.');
-    log.verbose(debug);
-  })
-  .catch((err) => {
-    log.error(err.message);
-    log.verbose(err);
-  });
+Promise.all([
+  events.load(),
+  bot.login(config.token),
+])
+.then((debug) => {
+  log.info('Le bot est maintenant connecté.');
+  log.verbose(debug);
+})
+.catch((err) => {
+  log.error(err.message);
+  log.verbose(err);
+});
 
