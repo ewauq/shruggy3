844c9a034338af488048e8aa3b0febf55b17e82e
Emilien <emilien.wauquier@gmail.com>
2017-05-24 00:43:59 +0200

    Ajouté la fonction random()

--- commands/index.js
+++ commands/index.js
@@ -1,4 +1,5 @@
 import Log from '../libs/log';
+import { random } from '../libs/random';
 
 const log = new Log();
 
@@ -8,10 +9,24 @@ export default class BaseCommand {
     this.name = name;
   }
 
+  /**
+   * Retourne le nom de la commande courrante.
+   *
+   * @returns {string} Le nom de la commande courrante.
+   *
+   * @memberof BaseCommand
+   */
   getName() {
     return this.name;
   }
 
+  /**
+   * Retourne les propriétés de la commande courrante.
+   *
+   * @returns {object} Les propriétés de la commande.
+   *
+   * @memberof BaseCommand
+   */
   getProperties() {
     let properties;
 
@@ -27,9 +42,34 @@ export default class BaseCommand {
     return properties;
   }
 
+  /**
+   * Choisit aléatoirement une réponse parmis les réponses données.
+   * Si la réponse contient une ou plusieurs expressions, celles-ci sont choisies
+   * aléatoirement puis la phrase est générée, et enfin retournée.
+   *
+   * @param {array} replies Les réponses disponibles.
+   * @returns {string} La réponse finale.
+   *
+   * @memberof BaseCommand
+   */
   getReply(replies) {
-    const rnd = Math.floor(Math.random() * replies.length);
-    return replies[rnd];
+    const final_reply = [];
+    const reply = replies[random(0, replies.length)];
+    const parts = reply.split(/(\(.+?\))/g);
+
+    parts.forEach((part) => {
+      if (part.match(/(\(.+?\))/)) {
+      // Si la phrase contient une expression.
+        const words = part.replace(/[{()}]/g, '').split('|');
+        final_reply.push(words[random(0, words.length)]);
+      } else {
+      // Sinon on la gère normalement.
+        final_reply.push(part);
+      }
+    });
+
+    return final_reply.join('');
   }
+
 
 }
---
+++ libs/random.js
@@ -0,0 +1,7 @@
+function random(min, max) {
+  return Math.floor(Math.random() * max);
+}
+
+export {
+  random,
+};
